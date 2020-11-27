8094d01d47ab5cbe2fee92d525a9f6a967383aaa
huuviet208 <huuviet.208@gmail.com>
2020-11-27 13:58:50 +0700

    Tao Dart_lab1

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dart Lab1:
+Nguyễn Hữu Việt
+1721050602
+#------------------------------------------------
+## Screenshots:
+
+### Hello Dart:
+![Logo TechMaster](screenshots/Sc1.png)
+
+### Nhiều lời chào:
+![Logo TechMaster](screenshots/Sc2.png)
+
+### Nạp thư viên:
+![Logo TechMaster](screenshots/Sc3.png)
---
+++ bin/dart_lab1.dart
@@ -0,0 +1,13 @@
+import 'dart:math';  //Nạp thư viện toán
+
+void main() {
+//  print('Hello, Dart!');
+//-------------------------
+/*  print('Xin chào, A');
+    print('Xin chào, B');  
+    print('Xin chào, C');
+    print('Xin chào, D');  
+    print('Xin chào, E');     
+//-------------------------*/  
+    print(sqrt(9));
+}
