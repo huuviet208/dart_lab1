b9ae7e66574f1706731cc025f1aeb6d484e37f6a
huuviet208 <huuviet.208@gmail.com>
2020-11-27 14:16:56 +0700

    Update README.md

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ Nguyễn Hữu Việt
 
 ### Nạp thư viên:
 ![Logo TechMaster](screenshots/Sc3.png)
+
+### Lấy kiểu của đối tượng:
+![Logo TechMaster](screenshots/Sc4.png)
+
+### Xử lý các trường hợp ngoại lệ:
+![Logo TechMaster](screenshots/Sc5.png)
--- bin/dart_lab1.dart
+++ bin/dart_lab1.dart
@@ -9,5 +9,24 @@ void main() {
     print('Xin chào, D');  
     print('Xin chào, E');     
 //-------------------------*/  
-    print(sqrt(9));
+//  print(sqrt(9));
+//-------------------------
+//  var p = 9;
+//  print('Kiểu của p là ${p.runtimeType}');
+//-------------------------
+/*    var a = '10.0';
+    int num;
+    try {
+      int num = int.parse(a);
+    print(num);
+    } catch (e) {
+      print(e.toString());
+    }
+//-------------------------*/ 
+    var a ='có';
+    final b = 'Dart $a phân biệt chữ hoa và chữ thường'; 
+    const c = 'Dart có phân biệt chữ hoa và chữ thường';
+    print(b);
+    a = 'không';
+    print(b);  
 }
